246961575d5348ed88851706949d075afe3340b9
GreatBam <108512439+GreatBam@users.noreply.github.com>
2024-03-05 09:46:32 +0100

    Add comments

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ from classes.round import Round
 from classes.score import Score
 
 def main():
-
+    # ask for player's name
     player_name = input('Enter your name: ')
+    # create player and computer
     player = Player(player_name)
     computer = Player("computer")
     players = [player, computer]
+    # create deck
     deck = Deck(players)
+    # start  round
     round =Round(player.hand, computer.hand, deck)
     round.show_player_hand()
+    # ask player for cards to change
     round.ask_cards_change()
-    
+    # show computer hand
     print('Computer hand is:')
     round.show_computer_hand()
     
