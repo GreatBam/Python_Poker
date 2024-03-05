12eaa553f7400c81d66767b0e256d3b54d5ca5fc
GreatBam <108512439+GreatBam@users.noreply.github.com>
2024-03-05 07:51:35 +0100

    Gice Card params type

--- classes/card.py
+++ classes/card.py
@@ -1,5 +1,5 @@
 class Card:
-    def __init__(self, suit, rank, img_path):
+    def __init__(self, suit:str, rank:str, img_path:str):
         self.suit = suit
         self.rank = rank
         self.img_path = img_path
