4cec378ea48008f7f2cf0a5108e80a1efb6210ec
Jonathan <jonathan.gabioud@hotmail.com>
2024-03-05 19:47:15 +0100

    Create card_change method in Button class

--- classes/button.py
+++ classes/button.py
@@ -20,5 +20,14 @@ class Button:
         self.text_position = ((self.width/2)+61,((self.height/2)+(self.height/4))-5)
         
     def draw(self):
-        pygame.draw.rect(self.screen, self.dark_color, [self.button_position[0], self.button_position[1], 140, 40], 0, 10)
+        self.button = pygame.draw.rect(self.screen, self.dark_color, [self.button_position[0], self.button_position[1], 140, 40], 0, 10)
         self.screen.blit(self.text, self.text_position)
+        
+    def card_change(self):
+        for card in self.player_hand:
+            if card.selected:
+                card.selected = False
+                self.player_hand.remove(card)
+                self.player_hand.append(self.deck.cards[0])
+                self.deck.cards.pop(0)
+        self.deck.display()
