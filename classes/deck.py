a43e5bd6ce260b15ca04c30c990183581ddf5017
GreatBam <108512439+GreatBam@users.noreply.github.com>
2024-03-05 08:10:30 +0100

    Improve deck creation in Deck class

--- classes/deck.py
+++ classes/deck.py
@@ -1,10 +1,10 @@
 import random, re
-from card import Card
+from classes.card import Card
 
 class Deck:
-    def __init__(self, cards:list):
-        self.cards = cards
-        self.create_deck()
+    def __init__(self):
+        self.cards = self.create_deck()
+        self.shuffle()
         
     def shuffle(self):
         random.shuffle(self.cards)
@@ -12,9 +12,10 @@ class Deck:
     def create_deck(self):
         suits_list = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
         ranks_list = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        suit = (re.findall(r'\b\w', suits_list)[0]).lower()
-        rank = (re.findall(r'\b\w', ranks_list)[0]).lower()
+        cards = []
         for suit in suits_list:
             for rank in ranks_list:
-                self.cards.append(Card(suit, rank, f'images/{rank}{suit}.png'))
-        self.shuffle()
+                suit_path = (re.findall(r'\b\w', suit)[0]).lower()
+                rank_path = (re.findall(r'\b\w', rank)[0]).lower()
+                cards.append(Card(suit, rank, f'images/{rank_path}{suit_path}.png'))
+        return cards
--- main.py
+++ main.py
@@ -7,5 +7,9 @@ import random, re
 from classes.deck import Deck
 from classes.player import Player
 from classes.score import Score
-from classes.card import Card
+# from classes.card import Card
 
+deck = Deck()
+
+for card in deck.cards:
+    print(card.suit, card.rank, card.img_path)
