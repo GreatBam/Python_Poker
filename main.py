c030a71a951de0cca37e2edd0773c30ddac11b1a
GreatBam <108512439+GreatBam@users.noreply.github.com>
2024-03-05 07:58:27 +0100

    Start main with info and importation

---
+++ main.py
@@ -0,0 +1,11 @@
+# Python poker card game
+# Jonathan Gabioud
+# 2024-03-05
+
+# Importing libraries
+import random, re
+from classes.deck import Deck
+from classes.player import Player
+from classes.score import Score
+from classes.card import Card
+
