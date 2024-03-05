a5b5c5b6daebdd450f416d627eccc3d1d404abf0
GreatBam <108512439+GreatBam@users.noreply.github.com>
2024-03-05 09:56:31 +0100

    Add basic game loop from Pygame documentation, commented for now

--- main.py
+++ main.py
@@ -16,6 +16,28 @@ clock = pygame.time.Clock()
 running = True
 
 def main():
+    #pygame setup, under construction
+    # while running:
+    #     # poll for events
+    #     # pygame.QUIT event means the user clicked X to close your window
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             running = False
+
+    #     # fill the screen with a color to wipe away anything from last frame
+    #     screen.fill("purple")
+
+    #     # RENDER YOUR GAME HERE
+
+    #     # flip() the display to put your work on screen
+    #     pygame.display.flip()
+
+    #     clock.tick(60)  # limits FPS to 60
+
+    # pygame.quit()
+    
+    # /////////////// GAME STARTS HERE FOR NOW /////////////// *
+    
     # ask for player's name
     player_name = input('Enter your name: ')
     # create player and computer
