f2a66de3acd5ecdcedcc98548f64138c36fb957e
Jonathan <jonathan.gabioud@hotmail.com>
2024-03-05 19:51:16 +0100

    Remove old parts from terminal game elaboration

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import pygame
 from classes.deck import Deck
 from classes.player import Player
 from classes.button import Button
-from classes.round import Round
 from classes.score import Score
 
 def main():
@@ -19,7 +18,6 @@ def main():
     running = True
     
     # game setup
-    # player_name = input('Enter your name: ') # under construction
     player = Player("player")
     computer = Player("computer")
     players = [player, computer]
@@ -64,22 +62,3 @@ def main():
 # run the game
 if __name__ == "__main__":
     main()
-
-# /////////////// TERMINAL GAME REMAINDER /////////////// *
-
-# # ask for player's name
-# player_name = input('Enter your name: ')
-# # create player and computer
-# player = Player(player_name)
-# computer = Player("computer")
-# players = [player, computer]
-# # create deck
-# deck = Deck(players)
-# # start  round
-# round =Round(player.hand, computer.hand, deck)
-# round.show_player_hand()
-# # ask player for cards to change
-# round.ask_cards_change()
-# # show computer hand
-# print('Computer hand is:')
-# round.show_computer_hand()
