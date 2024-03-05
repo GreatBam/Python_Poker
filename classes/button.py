436a5e1dd29356adb1c782bcf6816afe251eb41b
Jonathan <jonathan.gabioud@hotmail.com>
2024-03-05 19:13:50 +0100

    Create Button class

---
+++ classes/button.py
@@ -0,0 +1,8 @@
+# Python poker card game
+# Jonathan Gabioud
+# 2024-03-05
+
+class Button:
+    def __init__(self, player_hand:list, deck:list):
+        self.player_hand = player_hand
+        self.deck = deck
