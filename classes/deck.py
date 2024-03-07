6b5942d2323d0ff72db6737bc377d50e8c2c5b96
GreatBam <108512439+GreatBam@users.noreply.github.com>
2024-03-07 11:08:48 +0100

    Remove unused cards list in Deck create_deck method

--- classes/deck.py
+++ classes/deck.py
@@ -17,7 +17,6 @@ class Deck:
     def create_deck(self):
         suits_list = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
         ranks_list = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        cards = []
         for suit in suits_list:
             for rank in ranks_list:
                 suit_path = (re.findall(r'\b\w', suit)[0]).lower()
