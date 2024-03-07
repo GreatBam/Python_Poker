9a24ed11c8002e364b36baac14ef479d2b46a375
GreatBam <108512439+GreatBam@users.noreply.github.com>
2024-03-07 10:19:04 +0100

    Create Change class

---
+++ classes/change.py
@@ -0,0 +1,6 @@
+class Change:
+    def __init__(self, player_selection:list, player_hand:list, deck:object, display:object):
+        self.player_selection = player_selection
+        self.player_hand = player_hand
+        self.deck = deck
+        self.display = display
