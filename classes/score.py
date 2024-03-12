8e62f5fb75499f0bd73b38fbbbdf851d80c24e65
GreatBam <108512439+GreatBam@users.noreply.github.com>
2024-03-12 08:53:41 +0100

    Remove commented code in check_poker_hand method in Score class

--- classes/score.py
+++ classes/score.py
@@ -12,24 +12,6 @@ class Score:
     def check_poker_hand(self, hand:list):
         suits = [card.suit for card in hand]
         ranks = [card.rank for card in hand]
-        # if(self.straight_flush(suits, ranks)):
-        #     return "straight flush"
-        # elif(self.four_of_a_kind(ranks)):
-        #     return "four of a kind"
-        # elif(self.full_house(ranks)):
-        #     return "full house"
-        # elif(self.flush(suits)):
-        #     return "flush"
-        # elif(self.straight(ranks)):
-        #     return "straight"
-        # elif(self.three_of_a_kind(ranks)):
-        #     return "three of a kind"
-        # elif(self.two_pair(ranks)):
-        #     return "two pair"
-        # elif(self.pair(ranks)):
-        #     return "pair"
-        # else:
-        #     return "high card"
         print(self.rank_counter(ranks, suits))
         
     def rank_counter(self, ranks:list, suits:list):
